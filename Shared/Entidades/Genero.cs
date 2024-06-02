6290a1a3b63e117646f06a6c64dd625b5dc8ca56
Hawerd <hawerdgutierrez@gmail.com>
2024-06-02 13:14:43 -0400

    Añadida navegación y mejoras de formulario en componentes Genero
    Se han realizado varias mejoras y adiciones en los componentes `CrearGenero.razor`, `EditarGenero.razor` y `FormularioGenero.razor`. Se ha inyectado `NavigationManager` para permitir la navegación entre páginas y `IJSRuntime` para usar JavaScript. Se ha cambiado el título de la página de "Crear Genero" a un encabezado h3 y se ha añadido una referencia a `FormularioGenero` para acceder a sus métodos y propiedades. También se ha añadido una nueva propiedad `formularioGenero` para almacenar la referencia a `FormularioGenero` y una nueva línea de código en los métodos `Crear` y `Editar` para marcar el formulario como exitosamente posteado. Después de crear o editar un género, se redirige a la página "/generos". Se ha modificado `NavigationLock` para manejar el evento `OnBeforeInternalNavigation` y se ha cambiado el modelo del `EditForm` a `editContext`. Se ha añadido una nueva propiedad `FormularioPosteadoExitosamente` para indicar si el formulario ha sido posteado exitosamente y un nuevo método `OnBeforeInternalNavigation` para prevenir la navegación si el formulario ha sido editado pero no posteado. Finalmente, se ha añadido una validación de longitud mínima al campo `Nombre` en la clase `Genero`.

--- Shared/Entidades/Genero.cs
+++ Shared/Entidades/Genero.cs
@@ -5,6 +5,7 @@ public class Genero
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
+    [MinLength(3, ErrorMessage = "El campo {0} debe tener al menos {1} caracteres")]
     public string? Nombre { get; set; }
     public string? Descripcion { get; set; }
 
