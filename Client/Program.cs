fbfc1cd9de3aad042ea2ee045688e5be82552641
Hawerd <hawerdgutierrez@gmail.com>
2024-06-02 21:28:53 -0400

    Añadida la biblioteca SweetAlert2 y actualizada la lógica de confirmación
    Se ha incorporado la biblioteca `CurrieTechnologies.Razor.SweetAlert2` versión `5.6.0` al proyecto. Se ha reemplazado la inyección de dependencia `IJSRuntime js` por `SweetAlertService swal` en `FormularioGenero.razor`, modificando la lógica de confirmación de salida del formulario para usar `FireAsync` de `SweetAlertService`. En `Program.cs`, se ha añadido `using CurrieTechnologies.Razor.SweetAlert2;` y `builder.Services.AddSweetAlert2();` para registrar `SweetAlert2` en el contenedor de inyección de dependencias, y se ha registrado `IRepositorio` como un servicio singleton. En `_Imports.razor`, se ha añadido `@using CurrieTechnologies.Razor.SweetAlert2` para usar `SweetAlert2` en todos los componentes Razor. Finalmente, en `index.html`, se ha incluido el script de `SweetAlert2`.

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,16 +2,23 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorPeliculas.Client;
 using BlazorPeliculas.Client.Repositorios;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSweetAlert2();
+
 ConfigureServices(builder.Services);
+
+
+
 
 void ConfigureServices(IServiceCollection services)
 {
+ 
    services.AddSingleton<IRepositorio, Repositorio>();
 }
 
